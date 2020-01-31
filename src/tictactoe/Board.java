ce0f44af10ce053986605aea55c063d6dbf8e224
Wesley DeBrusk <wesleydebrusk@Wesleys-MacBook-Pro.local>
2020-01-30 21:32:29 -0500

    Basically done

--- src/tictactoe/Board.java
+++ src/tictactoe/Board.java
@@ -28,7 +28,32 @@ public class Board {
 		System.out.println("     |     |     ");
 	}
 
-	
+	// translate coordinate to a move on the board
+	public int getMoveNum(int i, int j) {
+		if (i == 0 && j == 0) {
+			return 1;
+		} else if (i == 0 && j == 1) {
+			return 2;
+		} else if (i == 0 && j == 2) {
+			return 3;
+		} else if (i == 1 && j == 0) {
+			return 4;
+		} else if (i == 1 && j == 1) {
+			return 5;
+		} else if (i == 1 && j == 2) {
+			return 6;
+		} else if (i == 2 && j == 0) {
+			return 7;
+		} else if (i == 2 && j == 1) {
+			return 8;
+		} else if (i == 2 && j == 2) {
+			return 9;
+		} else {
+			return 0;
+		}
+	}
+
+	// update board position
 	public void makeMove(int position, int playerNum) {
 		switch (position) {
 		case 1:
@@ -98,6 +123,7 @@ public class Board {
 
 	}
 
+	// return true if board position is empty
 	public boolean isMoveValid(int position) {
 		switch (position) {
 		case 1:
@@ -155,6 +181,139 @@ public class Board {
 				return false;
 			}
 		}
+		return false;
+	}
+
+	// returns the best possible move for the current state of the board
+	public int findBestMove() {
+		int bestVal = -1000;
+		int bestMove = -1;
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+
+				if (this.board[i][j] == " ") {
+
+					this.board[i][j] = "O";
+					int moveVal = minimax(0, false);
+					board[i][j] = " ";
+
+					if (moveVal > bestVal) {
+						bestVal = moveVal;
+						bestMove = getMoveNum(i, j);
+					}
+
+				}
+
+			}
+		}
+		return bestMove;
+	}
+
+	// return +10 if player 2 wins, -10 if player 1 wins, 0 if nobody wins
+	public int evaluate() {
+		// check rows
+		for (int row = 0; row < 3; row++) {
+			if (this.board[row][0] == this.board[row][1] && this.board[row][1] == this.board[row][2]) {
+				if (this.board[row][0] == "O")
+					return +10;
+				else if (this.board[row][0] == "X")
+					return -10;
+			}
+		}
+
+		// check columns
+		for (int col = 0; col < 3; col++) {
+			if (this.board[0][col] == this.board[1][col] && this.board[1][col] == this.board[2][col]) {
+				if (this.board[0][col] == "O")
+					return +10;
+
+				else if (this.board[0][col] == "X")
+					return -10;
+			}
+		}
+
+		// check diagonals
+		if (this.board[0][0] == this.board[1][1] && this.board[1][1] == this.board[2][2]) {
+			if (this.board[0][0] == "O")
+				return +10;
+			else if (this.board[0][0] == "X")
+				return -10;
+		}
+		if (this.board[0][2] == this.board[1][1] && this.board[1][1] == this.board[2][0]) {
+			if (this.board[0][2] == "O")
+				return +10;
+			else if (this.board[0][2] == "X")
+				return -10;
+		}	
+		
+		return 0;
+	}
+
+	public int minimax(int depth, Boolean playerTwoMove) {
+		int score = evaluate();
+ 
+		// if player 2 wins return +10
+		if (score == 10) {
+			return score;
+		}
+		
+		// if player 1 wins return -10
+		if (score == -10) {
+			return score;
+		}
+
+		// if there are no moves left then it's a tie
+		if (isMovesLeft() == false) {
+			return 0;
+		}
+
+		// if player 2 move
+		if (playerTwoMove) {
+			int best = -1000;
+			
+			// check each cell and use recursion if cell is empty
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+	
+					if (board[i][j] == " ") {
+						
+						board[i][j] = "O";
+						best = Math.max(best, minimax(depth + 1, !playerTwoMove));
+						board[i][j] = " ";
+					}
+				}
+			}
+			return best;
+		}
+
+		// if player 1 move
+		else {
+			int best = 1000;
+			
+			// check each cell and use recursion if empty
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					
+					if (board[i][j] == " ") {
+						
+						board[i][j] = "X";
+						best = Math.min(best, minimax(depth + 1, !playerTwoMove));
+						board[i][j] = " ";
+					}
+				}
+			}
+			return best;
+		}
+	}
+ 
+	// check if there are any moves left
+	public Boolean isMovesLeft() {
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				if (board[i][j] == " ") {
+					return true;
+				}
 		return false;
 	}
 
--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -5,26 +5,57 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
-		int mode;
 		Scanner kbd = new Scanner(System.in);
-		Board board = new Board();
+		while (true) {
+			int mode, keepPlaying;
+			
+			Board board = new Board();
 
-		System.out.println("Welcome to tic-tac-toe! Board positions are as follows: ");
-		board.printTutorial();
-		System.out.print("Enter 1 to play singleplay or 2 to play multiplayer: ");
-		mode = kbd.nextInt();
+			System.out.println("Welcome to tic-tac-toe! Board positions are as follows: ");
+			board.printTutorial();
+			System.out.print("Enter 1 to play singleplay or 2 to play multiplayer: ");
+			mode = kbd.nextInt();
 
-		switch (mode) {
-		case 1:
-			runSinglePlayer(board, kbd);
-		case 2:
-			runMultiplayer(board, kbd);
+			switch (mode) {
+			case 1:
+				runSinglePlayer(board, kbd);
+				break;
+			case 2:
+				runMultiplayer(board, kbd);
+				break;
 
+			}
+			
+			System.out.print("Enter 1 to play again, or anything else to quit: ");
+			keepPlaying = kbd.nextInt();
+			if (keepPlaying == 1) {
+				continue;
+			} else {
+				break;
+			}
 		}
-
+		System.out.println("Thanks for playing!");
 		kbd.close();
 
+	}
+
+	public static void clearScreen() {
+		System.out.println("\033\143");
+	}
+
+	public static boolean continueGame(Board board) {
+		if (board.evaluate() == 0 && board.isMovesLeft() == true) {
+			return true;
+		} else if (board.evaluate() == 10) {
+			System.out.println("Player 2 wins");
+			return false;
+		} else if (board.evaluate() == -10) {
+			System.out.println("Player 1 wins");
+			return false;
+		} else {
+			System.out.println("Tie");
+			return false;
+		}
 	}
 
 	public static void runSinglePlayer(Board board, Scanner kbd) {
@@ -35,10 +66,33 @@ public class Main {
 
 		while (true) {
 
-			System.out.print("Enter your move: ");
-			move = kbd.nextInt();
-			board.makeMove(move, 1);
-			board.printBoard();
+			if (continueGame(board)) {
+				System.out.print("Enter your move: ");
+				move = kbd.nextInt();
+				if (board.isMoveValid(move)) {
+					board.makeMove(move, 1);
+				} else {
+					while (true) {
+						System.out.print("Move not valid, enter another move: ");
+						move = kbd.nextInt();
+						if (board.isMoveValid(move)) {
+							board.makeMove(move, 1);
+							break;
+						}
+					}
+				}
+				board.printBoard();
+			} else {
+				return;
+			}
+
+			if (continueGame(board)) {
+				move = board.findBestMove();
+				board.makeMove(move, 2);
+				board.printBoard();
+			} else {
+				return;
+			}
 		}
 
 	}
@@ -48,39 +102,47 @@ public class Main {
 		board.printBoard();
 
 		while (true) {
-			System.out.print("Player 1 enter your move: ");
-			move = kbd.nextInt();
-			if (board.isMoveValid(move)) {
-				board.makeMove(move, 1);
-			} else {
-				while (true) {
-					System.out.print("Move not valid, enter another move: ");
-					move = kbd.nextInt();
-					if (board.isMoveValid(move)) {
-						board.makeMove(move, 1);
-						break;
+
+			if (continueGame(board)) {
+				System.out.print("Player 1 enter your move: ");
+				move = kbd.nextInt();
+				if (board.isMoveValid(move)) {
+					board.makeMove(move, 1);
+				} else {
+					while (true) {
+						System.out.print("Move not valid, enter another move: ");
+						move = kbd.nextInt();
+						if (board.isMoveValid(move)) {
+							board.makeMove(move, 1);
+							break;
+						}
 					}
 				}
-			}
-
-			board.printBoard();
-
-			System.out.print("Player 2 enter your move: ");
-			move = kbd.nextInt();
-			if (board.isMoveValid(move)) {
-				board.makeMove(move, 2);
+				board.printBoard();
 			} else {
-				while (true) {
-					System.out.print("Move not valid, enter another move: ");
-					move = kbd.nextInt();
-					if (board.isMoveValid(move)) {
-						board.makeMove(move, 2);
-						break;
+				return;
+			}
+			if (continueGame(board)) {
+				board.printBoard();
+
+				System.out.print("Player 2 enter your move: ");
+				move = kbd.nextInt();
+				if (board.isMoveValid(move)) {
+					board.makeMove(move, 2);
+				} else {
+					while (true) {
+						System.out.print("Move not valid, enter another move: ");
+						move = kbd.nextInt();
+						if (board.isMoveValid(move)) {
+							board.makeMove(move, 2);
+							break;
+						}
 					}
 				}
+				board.printBoard();
+			} else {
+				return;
 			}
-			board.printBoard();
-
 		}
 
 	}
